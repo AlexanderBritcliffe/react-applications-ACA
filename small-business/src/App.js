5abf0cddec0a68ad6c2d2ecaad6ce9a38d0cfb96
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2020-07-09 19:13:39 -0400

    test committ

--- small-business/src/App.js
+++ small-business/src/App.js
@@ -6,13 +6,12 @@ import Router from './Router'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from './redux/store'
-import cookie from 'cookie'
+
 
 function App() {
     return (
       <Provider store={store}>
         <BrowserRouter>
-        {console.log(document.cookie)}
           <Nav />
           <Router />
         </BrowserRouter>
